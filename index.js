73e4634d902a3707fa95b5d306246b56011f9b2d
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2023-01-06 20:07:20 -0300

    commit ...

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
+import fs from 'fs'
 import chalk from 'chalk';
 
-console.log(chalk.green('Hello world!'));
 
-console.log(chalk.greenBright('olá mundo'));
-console.log('ão geralmente recuperados a partir de um objeto [FileList](https://developer.mozilla.org/pt-BR/docs/Web/API/FileList) que é retornado como resultado da seleção, pelo usuário, de arquivos através do elemento [<input>](https://developer.mozilla.org/pt-BR/docs/Web/HTML/Element/Input), a partir do objeto [DataTransfer](https://developer.mozilla.org/pt-BR/docs/Web/API/DataTransfer) utilizado em operações de arrastar e soltar, ou a partir da API `mozGetAsFile()`');
-console.log('em um [HTMLCanvasElement](https://developer.mozilla.org/pt-BR/docs/Web/API/HTMLCanvasElement). Em Gecko, códigos com privilégiios podem criar objetos File representando qualquer arquivo local sem a intereção do usuário (veja [Implementation notes](https://developer.mozilla.org/pt-BR/docs/Web/API/File#implementation_notes) para mais informações.).');
+function trataErro(erro){
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'nao ha arquivo'));
+}
+
+// async/ await
+
+async function pegaArquivo(caminhoAqv){
+    try{
+        const encoding = 'utf-8'
+        const texto  =  await fs.promises.readFile(caminhoAqv, encoding)
+        console.log(chalk.green(texto))
+
+    }
+    catch(erro){
+        trataErro(erro)
+    }
+    
+}
+
+
+
+
+// promises com then()
+
+// function pegaArquivo(caminhoAqv){
+//     const encoding = 'utf-8'
+//     fs.promises.readFile(caminhoAqv, encoding)
+//         .then((texto) => console.log(chalk.green(texto)))
+//         .catch(trataErro)
+
+// }
+
+// function pegaArquivo(caminhoAqv){
+//     const encoding = 'utf-8'
+//     fs.readFile(caminhoAqv, encoding, (erro, texto) =>{
+//         if (erro){
+//             trataErro(erro);
+//         }
+
+//         console.log(chalk.green(texto))
+
+//     } )
+// }
+
+pegaArquivo('./arquivos/texto.md')
