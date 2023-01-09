4f471465f514cf4248906974698599657262246e
Pedro Amorim <86755845+Diposu@users.noreply.github.com>
2023-01-09 12:05:06 -0300

    commit ...

--- src/cli.js
+++ src/cli.js
@@ -5,26 +5,40 @@ import pegaArquivo from "./index.js";
 
 const caminho = process.argv
 
-function imprimeL (resultado){
-    console.log(chalk.yellow('lista de links'), resultado)
+function imprimeL (resultado, id = ''){
+    console.log(
+        chalk.yellow('lista de links'),
+        chalk.yellow.bgBlue(id),
+        resultado)
 
 
 }
 
 
 async function processaTexto(argumentos) {
-    const caminho =  argumentos[2]
+    const caminho =  argumentos[2];
+
+    try{
+        fs.lstatSync(caminho);
+
+    }catch(erro){
+        if(erro.code === 'ENOENT'){
+            console.log(chalk.red('arquivo ou diretorio nao existe'))
+            return;
+        }
+
+    }
 
     if (fs.lstatSync(caminho).isFile()){
         const resultados = await pegaArquivo(caminho)
-        imprimeL(resultados)
+        imprimeL(resultados,  )
 
     }
     else if (fs.lstatSync(caminho).isDirectory()){
         const arquivo = await fs.promises.readdir(caminho)
         arquivo.forEach(async (nomeaqv) => {
             const lista = await pegaArquivo(`${caminho}/${nomeaqv}`)
-            imprimeL(lista)
+            imprimeL(lista, nomeaqv)
         })
     }
 
